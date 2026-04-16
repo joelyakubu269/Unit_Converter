cc82f7656595eec625173b50875ae6c7d7f5fcec
Joel Yakubu <joelyakubu269@gmail.com>
2026-04-16 01:28:44 +0100

    backend and html forms

--- unitConverter/main.go
+++ unitConverter/main.go
@@ -13,35 +13,44 @@ type Result struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	if r.Method == "http.MethodPost" {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	if r.Method == http.MethodPost {
 		value := r.FormValue("value")
 		from := r.FormValue("from")
 		to := r.FormValue("to")
+
 		num, err := strconv.ParseFloat(value, 64)
 		if err != nil {
 			http.Error(w, "Invalid input", http.StatusBadRequest)
 			return
 		}
+
 		var result float64
-		if from == "meters" && to == "kilometres" {
-			result = num / 1000
-		} else if from == "kilometres" && to == "meters" {
-			result = num * 1000
-		} else if from == "meters" && to == "centimeters" {
-			result = num * 100
-		} else if from == "centimeters" && to == "meters" {
-			result = num / 100
+
+		if from == "Celsius" && to == "Fahrenheit" {
+			result = (num * 9 / 5) + 32
+		} else if from == "Celsius" && to == "Kelvin" {
+			result = num + 273.15
+		} else if from == "Fahrenheit" && to == "Celsius" {
+			result = (num - 32) * 5 / 9
+		} else if from == "Fahrenheit" && to == "Kelvin" {
+			result = (num-32)*5/9 + 273.15
+		} else if from == "Kelvin" && to == "Celsius" {
+			result = num - 273.15
+		} else if from == "Kelvin" && to == "Fahrenheit" {
+			result = (num-273.15)*9/5 + 32
 		}
+
 		data := Result{
 			Input:  value + " " + from,
 			Output: fmt.Sprintf("%.4f %s", result, to),
 		}
+
 		tmpl.Execute(w, data)
 		return
-
 	}
-	http.ServeFile(w, r, "index.html")
+
 	tmpl.Execute(w, nil)
 }
 func main() {
