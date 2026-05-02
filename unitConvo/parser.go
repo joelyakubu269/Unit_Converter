e312aacf53e9cff843401ad04cb3f457e74d9fb4
Stanley-0ps <ochaistanley7@gmail.com>
2026-05-02 08:40:22 +0100

    giving my project a proper structure

--- unitConvo/parser.go
+++ unitConvo/parser.go
@@ -1,7 +1,12 @@
 package main
 
-func parseForm(r *http.Request) (float64,string,string,string,error) {
-	if r.Method!= http.MethodPost {
-		
+import (
+	"fmt"
+	"net/http"
+)
+
+func parseForm(r *http.Request) (float64, string, string, string, error) {
+	if r.Method != http.MethodPost {
+		return 0,"","","",fmt.Errorf("only post method allowed")
 	}
 }
