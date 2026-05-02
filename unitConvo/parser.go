f955318154110dc687c747d1ad8999b8789529e3
Stanley-0ps <ochaistanley7@gmail.com>
2026-05-02 09:03:55 +0100

    wrote the parser function to extract values from the form and handle errors

--- unitConvo/parser.go
+++ unitConvo/parser.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func parseForm(r *http.Request) (float64, string, string, string, error) {
-	if r.Method != http.MethodPost {
-		return 0,"","","",fmt.Errorf("only post method allowed")
+	if err := r.ParseForm(); err != nil { // r.ParseForm() is used to get all values in name
+		return 0, "", "", "", fmt.Errorf("error getting values") // is using this error wrapper okay
 	}
+	value := r.FormValue("value")
+	from := r.FormValue("from")
+	to := r.FormValue("to")
+	convType := r.FormValue("type")
+	n, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return 0, "", "", "", fmt.Errorf("invalid Number")
+	}
+	return n, from, to, convType, err
 }
