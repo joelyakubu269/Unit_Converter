0d9c38860ab887558520d1d4ee24b19fe62fe104
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-11 03:46:06 +0100

    making use of structs and maps to refactor my code

--- unitConvo/service.go
+++ unitConvo/service.go
@@ -18,6 +18,49 @@ func convert(n float64, from, to, convtype string) (float64, error) {
 	}
 }
 
+var lengthunits = map[string]Unit{
+	"metres": {
+		ToBase:   func(n float64) float64 { return n },
+		FromBase: func(n float64) float64 { return n },
+	},
+	"centimetres": {
+		ToBase:   func(n float64) float64 { return n * 100 },
+		FromBase: func(n float64) float64 { return n / 100 },
+	},
+	"kilometres": {
+		ToBase:   func(n float64) float64 { return n * 1000 },
+		FromBase: func(n float64) float64 { return n / 1000 },
+	},
+}
+var weightUnits = map[string]Unit{
+	"kilograms": {
+		ToBase:   func(n float64) float64 { return n },
+		FromBase: func(n float64) float64 { return n },
+	},
+	"grams": {
+		ToBase:   func(n float64) float64 { return n / 1000 },
+		FromBase: func(n float64) float64 { return n * 1000 },
+	},
+	"pounds": {
+		ToBase:   func(n float64) float64 { return n * 0.453592 },
+		FromBase: func(n float64) float64 { return n * 2.20462 },
+	},
+}
+var tempUnits = map[string]Unit{
+	"celsius": {
+		ToBase:   func(n float64) float64 { return n },
+		FromBase: func(n float64) float64 { return n },
+	},
+	"kelvin": {
+		ToBase:   func(n float64) float64 { return n - 273.15 },
+		FromBase: func(n float64) float64 { return n + 273.15 },
+	},
+	"fahrenheit": {
+		ToBase:   func(n float64) float64 { return (n - 32) * 5 / 9 },
+		FromBase: func(n float64) float64 { return (n * 9 / 5) + 32 },
+	},
+}
+
 func convertTemperature(n float64, from, to string) (float64, error) {
 	var result float64
 
