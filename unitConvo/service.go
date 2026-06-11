bca204a4e6f5fe9fc575c9b5ad8b4eb8e62d27b4
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-11 04:26:23 +0100

    making one generic function for all units

--- unitConvo/service.go
+++ unitConvo/service.go
@@ -18,12 +18,19 @@ func convert(n float64, from, to, convtype string) (float64, error) {
 	}
 }
 func convertGeneric(n float64, from, to string, units map[string]Unit) (float64, error) {
+	if from == to {
+		return n, nil
+	}
 	fromUnit, ok := units[from]
 	if !ok {
 		return 0, fmt.Errorf("invalid unit: %s does not exist", from)
 	}
 	toUnit, ok := units[to]
-
+	if !ok {
+		return 0, fmt.Errorf("invalid unit: %s does not exist", to)
+	}
+	base := fromUnit.ToBase(n)
+	return toUnit.FromBase(base), nil
 }
 
 var lengthunits = map[string]Unit{
