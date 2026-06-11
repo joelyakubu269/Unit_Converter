c1b2bcdeb2bebbec223a4a742ab3c82d384cb1e4
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-11 01:07:49 +0100

    refactoring my code

--- unitConvo/main.go
+++ unitConvo/main.go
@@ -19,7 +19,7 @@ func handleLength(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	n, from, to, err := parseValue(r)
+
 	if err != nil {
 		http.Error(w, "badrequest", http.StatusBadRequest)
 		return
@@ -145,17 +145,18 @@ func handleTemp(w http.ResponseWriter, r *http.Request) {
 		"result": result,
 	})
 }
-func parseValue(r *http.Request) (float64, string, string, error) {
+
+func parseForm(r *http.Request) (float64, string, string, error) {
 
 	value := r.FormValue("value")
 	from := r.FormValue("from")
 	to := r.FormValue("to")
-
+	//convType := r.FormValue("type")
 	n, err := strconv.ParseFloat(value, 64)
 	if err != nil {
-		return 0, "", "", fmt.Errorf("invalid number: %v", err)
+		return 0, "", "", fmt.Errorf("invalid Number")
 	}
-	return n, from, to, nil
+	return n, from, to, err
 }
 
 func main() {
